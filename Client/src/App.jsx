1820e318c130c986d149cbc0b5af662161035554
Achintya Gulati <achintyaa.26@gmail.com>
2025-02-09 17:00:41 +0530

    Case fixed issue

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
-import GameRoom from "./pages/GameRoom";
+import GameRoom from "./pages/Gameroom";
 import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
